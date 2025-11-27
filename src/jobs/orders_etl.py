9efed92f6b7a251224d2e3da91d320d40dda4990
William <willdzai@LAPTOP-7F3SU4KO.>
2025-11-27 11:26:25 +0700

    feat: Add Robust CI/CD with ECR images

--- src/jobs/orders_etl.py
+++ src/jobs/orders_etl.py
@@ -4,17 +4,16 @@ from pyspark.sql.functions import from_json, col
 from pyspark.sql.types import StructType, StringType, DoubleType, IntegerType
 
 def main():
-    print("🚀 CI/CD SPARK JOB STARTED............")
+    print("🚀 CI/CD SPARK JOB STARTED...")
     
     spark = SparkSession.builder.appName("CICD_Orders_ETL").getOrCreate()
     spark.sparkContext.setLogLevel("WARN")
 
-    # Config (Internal DNS)
+    # Config
     KAFKA = "kafka-controller-0.kafka-controller-headless.ingestion.svc.cluster.local:9092"
     BUCKET = "lakehouse"
 
     # 1. Read
-    print(f"🔌 Connecting to Kafka: {KAFKA}")
     df = spark.readStream.format("kafka") \
         .option("kafka.bootstrap.servers", KAFKA) \
         .option("subscribe", "orders") \
@@ -26,8 +25,7 @@ def main():
 
     parsed_df = df.select(from_json(col("value").cast("string"), schema).alias("data")).select("data.*")
 
-    # 3. Write to MinIO
-    # Note: We use the S3A protocol
+    # 3. Write
     query = parsed_df.writeStream.format("parquet") \
         .option("path", f"s3a://{BUCKET}/bronze/orders/") \
         .option("checkpointLocation", f"s3a://{BUCKET}/checkpoints/orders_cicd/") \
